09ea192891792d92d77295d0a9bfc577ca48afa9
Taraldinn <kferdoush617@gmail.com>
2022-12-20 17:22:50 +0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+
+
+<template>
+  <h1 class="font-mono text-center text-3xl ">{{title}}</h1>
+  <div v-if="showModel">
+    <Modal :text="text" :header="header" theme="sale" @close="toggleModel"/>
+
+  </div>
+  <div class="flex justify-center items-center">
+    <button class="py-2 px-5 bg-slate-900 text-white font-bold font-mono "
+            @click="toggleModel">Show Modal
+    </button>
+
+  </div>
+</template>
+
+<script>
+
+import Modal from "./components/Modal.vue";
+
+export default {
+  name: "App",
+  components: {Modal},
+  data() {
+    return {
+      title: "Hello World",
+      showModel: false,
+    };
+  },
+  methods: {
+    toggleModel() {
+      this.showModel = !this.showModel;
+    },
+
+  }
+
+}
+
+</script>
+
+
+
+<style>
+
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="top-0 fixed bg-[rgba(0,0,0,0.5)]  w-full h-full" @click.self="closeModal">
+    <div class="modal w-[400px] p-5 mx-auto my-20 bg-white rounded-2xl" :class="{ sale: theme === 'sale' }">
+      <p>{{header}}</p>
+      <p>{{text}}</p>
+      <p>{{theme}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Modal",
+  props: ["header", "text", "theme"],
+  methods: {
+    closeModal() {
+      this.$emit('close');
+    },
+  },
+}
+</script>
+
+<style >
+.sale{
+  background-color: rgba(255, 213, 1, 0.65);
+}
+</style>
